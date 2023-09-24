c8937a0bf04150bd8f07e150d485516084df0c75
wuenne <joerg@riechers.berlin>
2023-09-24 14:32:44 +0200

    test

---
+++ main.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    hello: sayHello(),
+    goodby: sayGoodBye(),
+
+}
+
+function sayHello() {
+   return "Hello Wuenne!";
+}
+
+function sayGoodBye() {
+    return "Bye bye Wuenne!";   
+}
--- wuenne.js
+++
@@ -1,9 +0,0 @@
-var 
-    hello = function hello() {
-        return "Hello Wuenne!";
-    };
-
-var
-    goodBye = function goodBye() {
-        return "Bye bye Wuenne!";   
-    };
